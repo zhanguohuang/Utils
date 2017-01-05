e0b5f5f24bf5027443e3ffd117888043f16ca13c
zhanguohuang <zhanguohuang@szhuarong.com>
2017-01-05 10:44:50 +0800

    提交xml接口

---
+++ src/org/zhan/xml/XmlUtil.java
@@ -0,0 +1,69 @@
+package org.zhan.xml;
+
+import java.util.Map;
+
+import org.w3c.dom.Document;
+
+/**
+ * XmlUtil 工具类
+ * 将对象转换为Document 
+ * 
+ */
+public class XmlUtil {
+	
+	public static final String DEFAULT_XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\">";
+
+	/**
+	 * 将对象转换成xmlDocument
+	 * @param obj
+	 * @return
+	 */
+	public static Document parseObject(Object obj) {
+		
+	}
+	
+	/**
+	 * 将对象转换为xmlDocument
+	 * @param obj
+	 * @param hasHeader 判断头部是否带有DEFAULT_XML_HEADER
+	 * @return
+	 */
+	public static Document parseObejct(Object obj, boolean hasHeader) {
+		
+	}
+	
+	/**
+	 * 将对象转换成xmlString
+	 * @param obj
+	 * @return
+	 */
+	public static String parseObjectToString(Object obj) {
+		
+	}
+	
+	/**
+	 * 将对象转换成xmlString
+	 * @param obj
+	 * @param hasHeader 判断头部是否带有DEFAULT_XML_HEADER
+	 * @return
+	 */
+	public static String parseObjectToString(Object obj, boolean hasHeader) {
+		
+	}
+	
+	public static Document parseMap(Map map) {
+		
+	}
+	
+	public static Document parseMap(Map map, boolean hasHeader) {
+		
+	}	
+	
+	public static String parseMapToString(Map map) {
+		
+	}
+	
+	public static String parseMapToString(Map map, boolean hasHeader) {
+		
+	}
+}
