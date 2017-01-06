122d17e805b38de2ef23fd8d30cea66bf8ce3013
zhanguohuang <zhanguohuang@szhuarong.com>
2017-01-06 11:09:29 +0800

    commit xmlUtil

--- src/org/zhan/socket/HttpServer.java
+++ src/org/zhan/socket/HttpServer.java
@@ -10,36 +10,35 @@ public class HttpServer{
     try { 
        port = Integer.parseInt(args[0]);
      }catch (Exception e) {
-       System.out.println("port = 80 (默认)");
-       port = 80; //默认端口为8080
+       System.out.println("port = 80 (绔彛)");
+       port = 80; //榛樿鐩戝惉80绔彛
      }
 
      try{
        serverSocket = new ServerSocket(port); 
-       System.out.println("服务器正在监听端口：" + serverSocket.getLocalPort());
+       System.out.println("" + serverSocket.getLocalPort());
         
-       while(true) { //服务器在一个无限循环中不断接收来自客户的TCP连接请求
+       while(true) { //
          try{
-           //等待客户的TCP连接请求
+           //
            final Socket socket = serverSocket.accept(); 
-           System.out.println("建立了与客户的一个新的TCP连接，该客户的地址为："+
+           System.out.println(""+
                 socket.getInetAddress()+":" + socket.getPort());
           
-           service(socket);  //响应客户请求
+           service(socket);  //
         }catch(Exception e){e.printStackTrace();} 
       } //#while
     }catch (Exception e) {e.printStackTrace();}
   }
 
-  /** 响应客户的HTTP请求 */
+  /** HTTP */
   public static void service(Socket socket)throws Exception{
-    /*读取HTTP请求信息*/
-    InputStream socketIn=socket.getInputStream(); //获得输入流
-    Thread.sleep(1000);  //睡眠1秒，等待HTTP请求  
+    InputStream socketIn=socket.getInputStream(); //
+    Thread.sleep(1000);  //
     int size=socketIn.available();
     byte[] requestBuffer=new byte[size];
     socketIn.read(requestBuffer);
     String request=new String(requestBuffer);
-    System.out.println(request); //打印HTTP请求数据  
+    System.out.println(request); //
     } 
 }
--- src/org/zhan/xml/XmlUtil.java
+++ src/org/zhan/xml/XmlUtil.java
@@ -2,7 +2,12 @@ package org.zhan.xml;
 
 import java.util.Map;
 
-import org.w3c.dom.Document;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.springframework.cglib.beans.BeanMap;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 /**
  * XmlUtil 工具类
@@ -11,25 +16,62 @@ import org.w3c.dom.Document;
  */
 public class XmlUtil {
 	
-	public static final String DEFAULT_XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\">";
-
+	public static final String DEFAULT_ROOT_ELEMENT = "root";
+	
 	/**
 	 * 将对象转换成xmlDocument
 	 * @param obj
 	 * @return
 	 */
 	public static Document parseObject(Object obj) {
-		
+		return parseObject(obj, null);
 	}
 	
 	/**
-	 * 将对象转换为xmlDocument
 	 * @param obj
-	 * @param hasHeader 判断头部是否带有DEFAULT_XML_HEADER
+	 * @param rootName 根节点名称
 	 * @return
 	 */
-	public static Document parseObejct(Object obj, boolean hasHeader) {
-		
+	public static Document parseObject(Object obj, String rootName) {
+		Assert.notNull(obj, "obj must not be null");
+		Document doc = DocumentHelper.createDocument();
+		rootName = StringUtils.isEmpty(rootName) ? DEFAULT_ROOT_ELEMENT : rootName;
+		Element root = doc.addElement(rootName);
+		createObjectElement(root, obj);
+		return doc;
+	}
+	
+	/**
+	 * @param parentElement 父节点
+	 * @param obj
+	 */
+	private static void createObjectElement(Element parentElement, Object obj) {
+		Assert.notNull(parentElement, "parentElement must not be null");
+		Assert.notNull(obj, "obj must not be null");
+		if(parentElement.isRootElement() && isSimpleClass(obj.getClass())) {
+			parentElement.addText(obj.toString());
+			return;
+		}
+		BeanMap beanMap = BeanMap.create(obj);
+		for(Object key : beanMap.keySet()) {
+			if(beanMap.get(key) == null) continue;
+			if(isSimpleClass(beanMap.get(key).getClass())) {
+				Element childElement = parentElement.addElement(key.toString());
+				childElement.addText(beanMap.get(key).toString());
+			} else {
+				Element childElement = parentElement.addElement(obj.getClass().getSimpleName());
+				createObjectElement(childElement, beanMap.get(key));
+			}
+		}
+	}
+	
+	/**
+	 * 判断传入的class是否为基本类型和在java.lang包下的对象
+	 * @param clazz
+	 * @return
+	 */
+	private static boolean isSimpleClass(Class clazz) {
+		return clazz.isPrimitive() || clazz.getName().matches("java.lang.\\w+"); 
 	}
 	
 	/**
@@ -38,7 +80,7 @@ public class XmlUtil {
 	 * @return
 	 */
 	public static String parseObjectToString(Object obj) {
-		
+		return parseObjectToString(obj, null);
 	}
 	
 	/**
@@ -47,23 +89,69 @@ public class XmlUtil {
 	 * @param hasHeader 判断头部是否带有DEFAULT_XML_HEADER
 	 * @return
 	 */
-	public static String parseObjectToString(Object obj, boolean hasHeader) {
-		
+	public static String parseObjectToString(Object obj, String rootName) {
+		return parseObject(obj, rootName).asXML();
 	}
 	
+	/**
+	 * 将map对象转换成xml
+	 * @param map
+	 * @return
+	 */
 	public static Document parseMap(Map map) {
-		
+		return parseMap(map, null);
 	}
 	
-	public static Document parseMap(Map map, boolean hasHeader) {
-		
+	/**
+	 * @param map
+	 * @param rootName 根节点名称
+	 * @return
+	 */
+	public static Document parseMap(Map map, String rootName) {
+		Assert.notNull(map, "map must not be null");
+		Document doc = DocumentHelper.createDocument();
+		rootName = StringUtils.isEmpty(rootName) ? DEFAULT_ROOT_ELEMENT : rootName;
+		Element root = doc.addElement(rootName);
+		createMapElement(root, map);
+		return doc;
 	}	
 	
-	public static String parseMapToString(Map map) {
-		
+	private static void createMapElement(Element parentElement, Map map) {
+		Assert.notNull(parentElement, "parentElement must not be null");
+		Assert.notNull(map, "map must not be null");
+		for(Object key : map.keySet()) {
+			Assert.isTrue(isSimpleClass(key.getClass()), "key must be basic data type");
+			Element childElement = parentElement.addElement(key.toString());
+			if(isSimpleClass(map.get(key).getClass())) {
+				childElement.addText(map.get(key).toString());
+			} else {
+				detectMapValue(childElement, map.get(key));
+			}
+			
+		}
 	}
 	
-	public static String parseMapToString(Map map, boolean hasHeader) {
-		
+	private static void detectMapValue(Element parentElement, Object obj) {
+		Assert.notNull(parentElement, "parentElement must not be null");
+		Assert.notNull(obj, "obj must not be null");
+		BeanMap beanMap = BeanMap.create(obj);
+		for(Object key : beanMap.keySet()) {
+			if(beanMap.get(key) == null) continue;
+			if(isSimpleClass(beanMap.get(key).getClass())) {
+				Element childElement = parentElement.addElement(key.toString());
+				childElement.addText(beanMap.get(key).toString());
+			} else {
+				detectMapValue(parentElement, beanMap.get(key));
+			}
+		}
 	}
+	
+	public static String parseMapToString(Map map) {
+		return parseMapToString(map, null);
+	}
+	
+	public static String parseMapToString(Map map, String rootName) {
+		return parseMap(map, rootName).asXML();
+	}
+	
 }
